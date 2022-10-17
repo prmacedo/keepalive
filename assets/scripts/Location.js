61b66cc827171f55d4111f60f231d107e42ce5c6
prmacedo <p.ricardomacedo@hotmail.com>
2022-10-17 10:41:17 -0300

    specify the address detail level

--- assets/scripts/Location.js
+++ assets/scripts/Location.js
@@ -14,15 +14,15 @@ class Location {
       const { latitude, longitude } = success.coords;
       const xhr = new XMLHttpRequest();
 
-      xhr.open('GET', `https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${latitude}&lon=${longitude}`);
+      xhr.open('GET', `https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${latitude}&lon=${longitude}&zoom=10`);
 
       xhr.addEventListener("load", () => {
         const response = JSON.parse(xhr.responseText);
-        const address = response.features[0].properties.address;
-        const { town } = address;
-        const uf = address['ISO3166-2-lvl4'].split('-')[1];
+        const properties = response.features[0].properties;
+        const city = properties.name;
+        const uf = properties.address['ISO3166-2-lvl4'].split('-')[1];
 
-        this.#location.textContent = `${town} - ${uf}`;
+        this.#location.textContent = `${city} - ${uf}`;
       });
 
       xhr.send();
